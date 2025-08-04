e5b3ddd860c819c54413b1f3dd5ef5cb6caee7aa
Hyoran <hyoran2000@gmail.com>
2025-08-04 20:44:57 -0300

    feat: adding h3 in app

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My first Page in Github Pages - Hyoran Heidrich</h2>
+      <h3>um texto legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
